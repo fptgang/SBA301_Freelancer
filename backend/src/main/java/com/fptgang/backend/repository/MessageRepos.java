4596928b138d6e9c2c548a31ec064fe3e68620ad
khavhuynhw <115153974+khavhuynhw@users.noreply.github.com>
2025-01-14 12:38:16 +0700

    SBA-41 Develop Message Service Repo

--- backend/src/main/java/com/fptgang/backend/repository/MessageRepos.java
+++ backend/src/main/java/com/fptgang/backend/repository/MessageRepos.java
@@ -2,10 +2,11 @@ package com.fptgang.backend.repository;
 
 import com.fptgang.backend.model.Message;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 @Repository
-public interface MessageRepos extends JpaRepository<Message,Long> {
+public interface MessageRepos extends JpaRepository<Message,Long>, JpaSpecificationExecutor<Message> {
     Optional<Message> findByMessageId(Long messageId);
 }
