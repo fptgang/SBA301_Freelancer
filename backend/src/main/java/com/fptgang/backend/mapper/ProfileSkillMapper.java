a9cf4c49c16db243196199d912846a13ee6f448c
khavhuynhw <115153974+khavhuynhw@users.noreply.github.com>
2025-01-16 20:33:59 +0700

    SBA-67 Implement ProfileMapper and ProfileSkillMapper

--- backend/src/main/java/com/fptgang/backend/mapper/ProfileSkillMapper.java
+++ backend/src/main/java/com/fptgang/backend/mapper/ProfileSkillMapper.java
@@ -1,22 +1,75 @@
 package com.fptgang.backend.mapper;
 
 import com.fptgang.backend.api.model.ProfileSkillDto;
-import com.fptgang.backend.model.ProfileSkill;
+import com.fptgang.backend.model.*;
+import com.fptgang.backend.repository.ProfileSkillRepos;
+import com.fptgang.backend.repository.SkillRepos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Optional;
 
 @Component
 public class ProfileSkillMapper extends BaseMapper<ProfileSkillDto, ProfileSkill> {
     @Autowired
     private SkillMapper skillMapper;
+    @Autowired
+    private SkillRepos skillRepos;
+
+    @Autowired
+    private ProfileSkillRepos profileSkillRepos;
 
     @Override
-    ProfileSkillDto toDTO(ProfileSkill entity) {
-        return null;
+    public ProfileSkillDto toDTO(ProfileSkill entity) {
+        if (entity == null) {
+            return null;
+        }
+
+        ProfileSkillDto dto = new ProfileSkillDto();
+
+        dto.setProfileSkillId(entity.getProfileSkillId());
+        dto.setSkill(skillMapper.toDTO(entity.getSkill()));
+        dto.setProficiency(ProfileSkillDto.ProficiencyEnum.valueOf(entity.getProficiency().name()));
+
+        return dto;
     }
 
     @Override
-    ProfileSkill toEntity(ProfileSkillDto dto) {
-        return null;
+    public ProfileSkill toEntity(ProfileSkillDto dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        Optional<ProfileSkill> existingEntityOptional = profileSkillRepos.findById(dto.getProfileSkillId());
+        if (existingEntityOptional.isPresent()) {
+            ProfileSkill existEntity = existingEntityOptional.get();
+
+            if (dto.getSkill() != null) {
+                existEntity.setSkill(findSkill(dto.getSkill().getSkillId()));
+            }
+
+            if (dto.getProficiency() != null) {
+                existEntity.setProficiency(Proficiency.valueOf(dto.getProficiency().name()));
+            }
+
+            return existEntity;
+        } else {
+            ProfileSkill entity = new ProfileSkill();
+
+            entity.setProfileSkillId(dto.getProfileSkillId());
+            if (dto.getSkill() != null) {
+                entity.setSkill(findSkill(dto.getSkill().getSkillId()));
+            }
+            if (dto.getProficiency() != null) {
+                entity.setProficiency(Proficiency.valueOf(dto.getProficiency().name()));
+            }
+
+            return entity;
+        }
+    }
+
+    public Skill findSkill(Long id) {
+        return skillRepos.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Skill does not exist"));
     }
 }
