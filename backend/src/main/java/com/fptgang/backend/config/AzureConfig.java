851e160c1319398a0df12e4cc7cba68920dfc555
vidz231 <115398831+vidz231@users.noreply.github.com>
2025-01-18 09:50:36 +0700

    test encode

--- backend/src/main/java/com/fptgang/backend/config/AzureConfig.java
+++ backend/src/main/java/com/fptgang/backend/config/AzureConfig.java
@@ -1,5 +1,6 @@
 package com.fptgang.backend.config;
 
+import com.azure.storage.blob.BlobServiceAsyncClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
 import com.azure.storage.common.StorageSharedKeyCredential;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
 
@@ -28,14 +31,15 @@ public class AzureConfig {
     private String storageAccountKey;
 
     @Bean
-    public BlobServiceClient blobServiceClient() {
+    public BlobServiceClient blobServiceClient() throws UnsupportedEncodingException {
         log.error(FileUtils.encodeBase64(storageAccountName.getBytes(StandardCharsets.UTF_8)));
         log.error(FileUtils.encodeBase64(storageEndpoint.getBytes(StandardCharsets.UTF_8)));
         log.error(FileUtils.encodeBase64(storageAccountKey.getBytes(StandardCharsets.UTF_8)));
 
         return new BlobServiceClientBuilder()
-                .endpoint(storageEndpoint)
+                .endpoint(URLEncoder.encode(storageEndpoint, StandardCharsets.UTF_8))
                 .credential(new StorageSharedKeyCredential(storageAccountName, storageAccountKey))
                 .buildClient();
     }
+
 }
