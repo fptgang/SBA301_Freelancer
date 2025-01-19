b22ad88c42c4d7785bb881875fbd661e7dec00fa
khavhuynhw <115153974+khavhuynhw@users.noreply.github.com>
2025-01-19 18:24:06 +0700

    SBA-71 Add refresh token clean task

--- backend/src/main/java/com/fptgang/backend/repository/RefreshTokenRepos.java
+++ backend/src/main/java/com/fptgang/backend/repository/RefreshTokenRepos.java
@@ -4,7 +4,12 @@ import com.fptgang.backend.model.RefreshToken;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.Instant;
 
 @Repository
 public interface RefreshTokenRepos extends JpaRepository<RefreshToken, Long> {
@@ -12,4 +17,8 @@ public interface RefreshTokenRepos extends JpaRepository<RefreshToken, Long> {
     Page<RefreshToken> findAllByAccount_AccountId(Long accountId, Pageable pageable);
     void deleteAllByAccount_Email(String email);
     void deleteAllBySessionId(String sessionId);
+
+    @Modifying
+    @Query("DELETE FROM RefreshToken rt WHERE rt.expiryDate < :now")
+    int deleteByExpiryDateBefore(@Param("now") Instant now);
 }
