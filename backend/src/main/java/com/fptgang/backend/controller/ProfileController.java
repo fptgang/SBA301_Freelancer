17a50d22c99ddeda3e81b615c287d9f0dc07c96e
khavhuynhw <115153974+khavhuynhw@users.noreply.github.com>
2025-01-20 14:07:58 +0700

    Update ProfileController.java

--- backend/src/main/java/com/fptgang/backend/controller/ProfileController.java
+++ backend/src/main/java/com/fptgang/backend/controller/ProfileController.java
@@ -3,10 +3,16 @@ package com.fptgang.backend.controller;
 import com.fptgang.backend.api.controller.ProfilesApi;
 import com.fptgang.backend.api.model.*;
 import com.fptgang.backend.mapper.ProfileMapper;
+import com.fptgang.backend.model.Account;
 import com.fptgang.backend.service.ProfileService;
+import com.fptgang.backend.util.OpenApiHelper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/profile")
+@Slf4j
 public class ProfileController implements ProfilesApi {
     private ProfileService profileService;
     private ProfileMapper profileMapper;
@@ -32,23 +39,27 @@ public class ProfileController implements ProfilesApi {
     }
 
     @Override
-    public ResponseEntity<Void> deleteProfile(Integer projectId) {
-        return ProfilesApi.super.deleteProfile(projectId);
+    public ResponseEntity<Void> deleteProfile(Integer profileId) {
+        profileService.deleteById(profileId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
     @Override
-    public ResponseEntity<ProfileDto> getProfileById(Integer projectId) {
-        return ProfilesApi.super.getProfileById(projectId);
+    public ResponseEntity<ProfileDto> getProfileById(Integer profileId) {
+        return new ResponseEntity<>(profileMapper.toDTO(profileService.findByProfileId(profileId)), HttpStatus.OK);
     }
 
     @Override
     public ResponseEntity<GetProfiles200Response> getProfiles(GetAccountsPageableParameter pageable, String filter) {
-        return ProfilesApi.super.getProfiles(pageable, filter);
+        log.info("Getting profiles");
+        var page = OpenApiHelper.toPageable(pageable);
+        var res = profileService.getAll(page, filter).map(profileMapper::toDTO);
+        return OpenApiHelper.respondPage(res, GetProfiles200Response.class);
     }
 
     @Override
     public ResponseEntity<ProfileDto> updateProfile(Integer profileId, ProfileDto profileDto) {
-        return ProfilesApi.super.updateProfile(profileId, profileDto);
+        return new ResponseEntity<>(profileMapper.toDTO(profileService.update(profileMapper.toEntity(profileDto))), HttpStatus.OK);
     }
 
 }
