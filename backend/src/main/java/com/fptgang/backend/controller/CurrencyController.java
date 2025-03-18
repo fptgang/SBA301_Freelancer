1894c70cf27a876948bd51237b4099a6b02bc437
khavhuynhw <115153974+khavhuynhw@users.noreply.github.com>
2025-03-19 00:03:33 +0700

    Hirable Config

--- backend/src/main/java/com/fptgang/backend/controller/CurrencyController.java
+++ backend/src/main/java/com/fptgang/backend/controller/CurrencyController.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 import java.util.Map;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("api/currencies")
 public class CurrencyController {
     private final CurrencyExchangeService currencyExchangeService;
 
