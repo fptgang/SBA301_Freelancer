fa7f4687f010dbef5a466b75fd68ba927a9c04be
Vĩ Lê <vi89012@gmail.com>
2025-03-26 06:29:29 +0700

    fix

--- backend/src/main/java/com/fptgang/backend/BackendApplication.java
+++ backend/src/main/java/com/fptgang/backend/BackendApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.time.ZoneOffset;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableScheduling
 public class BackendApplication {
